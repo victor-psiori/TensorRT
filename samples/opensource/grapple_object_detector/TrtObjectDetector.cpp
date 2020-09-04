e5d6b0a3aba7786e57de9907cacc09977fcdfe55
Victor George <victorgeorge@Victors-MBP.fritz.box>
2020-09-04 10:10:33 +0200

    basic structure of TrtObjectDetector class

--- samples/opensource/grapple_object_detector/TrtObjectDetector.cpp
+++ samples/opensource/grapple_object_detector/TrtObjectDetector.cpp
@@ -6,46 +6,45 @@
 #include <iostream>
 #include <sstream>
 
-std::ofstream p(engineFile.c_str(), std::ios::binary);
-IHostMemory* m_ModelStream = engine->serialize();
-p.write(reinterpret_cast<const char*>(m_ModelStream->data()), m_ModelStream->size());
-p.close();
-
-std::vector<char> trtModelStreamfromFile;	
-size_t size{ 0 };
-std::ifstream file(engine_file, std::ios::binary);
-if (file.good()) {
-	file.seekg(0, file.end);
-	size = file.tellg();
-	file.seekg(0, file.beg);
-	trtModelStreamfromFile.resize(size);
-	file.read(trtModelStreamfromFile.data(), size);
-	file.close();
-	//IRuntime* infer = createInferRuntime(gLogger.getTRTLogger());
-	engine_ = runtime_->deserializeCudaEngine(trtModelStreamfromFile.data(), size, nullptr);
-}
-
 class TrtObjectDetector{
 	public:
 	 TrtObjectDetector(const std::string filename) : 
 	 	engineFile(filename) {
-			 vector<char> trtModelStreamFromFile;
-			 size_t size{0};
 			 std::ifstream file(engineFile, std::ios::binary);
-			 if (file.good()) {
-				 file.seekg(0, file.end);
-				 size = file.tellg();
-				 file.seekg(0, file.beg);
-				 trtModelStreamFromFile.resize(size);
-				 file.read(trtModelStreamFromFile.data(), size);
-				 file.close();
-			}
+			 vector<char> trtModelStreamFromFile;
+			 extractContentsToBuffer(file, trtModelStreamFromFile);
+			 //TODO: steal the logging class from 
+			 //TensorRT/samples/common/logging.h
+			 IRuntime* runtime_ = createInferRuntime(gLogger.getTRTLogger());
+			 engine_ = runtime_->deserializeCudaEngine(trtModelStreamfromFile.data(),
+																									size, 
+																									nullptr);
+			
 		}
 
 	private:
 		string engineFile;
 		// TensorRT engine used to run the network
-		std::unique_ptr<nvinfer1::ICudaEngine> mEngine; 
+		std::unique_ptr<nvinfer1::ICudaEngine> mEngine;
+
+		/**
+		 * @brief function extracts content in .bin engine file to char vector
+		 * @param file: ifstream object to which .bin engineFile is loaded in bin mode
+		 * @param buf: char buffer to extracts contents of file
+		 */
+		void extractContentsToBuffer(std::ifstream& file, 
+																std::vector <char>& buf) {
+			size_t size{0};
+			if (file.good()) {
+				file.seekg(0, file.end);
+				size = file.tellg();
+				file.seekg(0, file.beg);
+				trtModelStreamFromFile.resize(size);
+				file.read(trtModelStreamFromFile.data(), size);
+				file.close();
+			}
+		}
+
 		
 
 
